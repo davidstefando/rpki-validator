4695b7405d6f74703127919a80336782399d0e4e
David Pratama <davidadityapratama@gmail.com>
2023-07-23 06:35:10 +0700

    WIP Route Validator

--- app/Http/Controllers/RoaCheckController.php
+++ app/Http/Controllers/RoaCheckController.php
@@ -3,15 +3,121 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RoaCheckRequest;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\Console\Input\Input;
+use function PHPUnit\Framework\isNull;
 
 class RoaCheckController extends Controller
 {
     public function index(RoaCheckRequest $request)
     {
+        $prefix = $request->prefix;
+        $originAs = $request->as;
+
+        // first try lookup OriginAS from ROTO API
+        $bgpOriginByAPI = $this->getBgpOriginAndBestMatchPrefix($request->prefix);
+        if($bgpOriginByAPI != null){
+            $prefix = $bgpOriginByAPI["prefix"];
+            $originAs = $bgpOriginByAPI["originAS"];
+        }
+
+        // if request including OriginAS use it instead
+        if($request->has('as') && ($request->input('as') != "")){
+            $originAs = $request->as;
+        }
+
+        // if none above solved OriginAS
+        if($originAs == null){
+            $error['as'] = "Can't find BGP Origin AS, please input AS";
+
+            return redirect()->back()
+                ->withErrors($error)
+                ->withInput($request->input());
+        }
+
+        $roaValidation = $this->validateRoute($prefix, $originAs);
+
         return view('web.roaCheck', [
-            'prefix' => $request->prefix,
-            'as' => $request->as
+            'prefix' => $prefix,
+            'as' => $originAs,
+            'roaValidation' => $roaValidation,
+            'APIStatus' => $this->getAPIStatus()
         ]);
+    }
+
+    private function getAPIStatus()
+    {
+        try{
+            $statusAPI = Http::get("https://rpki.idnic.net/api/v1/status");
+            if($statusAPI->successful()) {
+                $response = $statusAPI->object();
+
+                $rpkiLastUpdate =  Carbon::parse($response->lastUpdateDone)->setTimezone("Asia/Jakarta");
+                return [
+                    'rpkiLastUpdate' => $rpkiLastUpdate->format("Y-m-d H:i:s"),
+                    'rpkiLastUpdateFromNow' => $rpkiLastUpdate->diffForHumans()
+                ];
+            }
+
+            return false;
+
+        } catch (\Exception $exception){
+            Log::error($exception->getMessage() . " On Line " . $exception->getFile() . " " . $exception->getLine());
+            return false;
+        }
+    }
+
+    private function getBgpOriginAndBestMatchPrefix($prefix)
+    {
+        try {
+            $bgpAPI = Http::get("https://rest.bgp-api.net/api/v1/prefix/$prefix/search");
+            if($bgpAPI->successful()){
+                $bgpAPIResponse = $bgpAPI->object();
+
+                // if prefix not found API will return null
+                if($bgpAPIResponse == null){
+                    return null;
+                }
+
+                // if prefix not found in BGP
+                if($bgpAPIResponse->result->prefix == null){
+                    return null;
+                }
+
+                return [
+                    "prefix" => $bgpAPIResponse->result->prefix,
+                    "originAS" => $bgpAPIResponse->result->meta[0]->originASNs[0] ?? null
+                ];
+            } else {
+                return false;
+            }
+        } catch (\Exception $exception){
+            Log::error($exception->getMessage() . " On Line " . $exception->getFile() . " " . $exception->getLine());
+            return false;
+        }
+    }
+
+    private function validateRoute($prefix, $originAS){
+        try {
+            $rovAPI = Http::get("https://rpki.idnic.net/api/v1/validity/$originAS/$prefix");
+            if($rovAPI->successful()){
+                $rovAPIResponse = $rovAPI->object();
+                collect($rovAPIResponse->validated_route->validity->VRPs)->each(function (){
+
+                });
+                return [
+                    'status' => $rovAPIResponse->validated_route->validity->state,
+                    'description' => $rovAPIResponse->validated_route->validity->description,
+                ];
+            } else {
+                return false;
+            }
+        } catch (\Exception $exception){
+            Log::error($exception->getMessage() . " On Line " . $exception->getFile() . " " . $exception->getLine());
+            return false;
+        }
     }
 }
--- resources/views/web/roaCheck.blade.php
+++ resources/views/web/roaCheck.blade.php
@@ -26,15 +26,33 @@
         </div>
     </form>
 
-    <div class="mt-12   italic">
-        Results for 203.119.13.0/24 - 63515
-    </div>
+    @isset($roaValidation)
+        <div class="mt-12   italic">
+            Results for {{ $prefix }} - {{ $as }}
+        </div>
 
-    <div class="flex items-center justify-center mt-4 h-20 w-64 bg-valid rounded-full">
-        <span class="text-white text-3xl">Valid</span>
-    </div>
+        @if($roaValidation["status"] == "valid")
+            <div class="flex items-center justify-center mt-4 h-20 w-64 bg-valid rounded-full">
+                <span class="text-white text-3xl">Valid</span>
+            </div>
+        @endif
+
+        @if($roaValidation["status"] == "not-found")
+            <div class="flex items-center justify-center mt-4 h-20 w-64 bg-idnic-blue rounded-full">
+                <span class="text-white text-3xl">Not Found</span>
+            </div>
+        @endif
+
+        @if($roaValidation["status"] == "invalid")
+            <div class="flex items-center justify-center mt-4 h-20 w-64 bg-invalid rounded-full">
+                <span class="text-white text-3xl">Invalid</span>
+            </div>
+        @endif
+    @endisset
 @endsection
 
 @section('footer')
-    <span class="text-gray-400 text-sm">Last updated </span>
+    <span class="text-gray-400 text-sm">ROV data last updated
+        <span class="font-bold">{{ $APIStatus['rpkiLastUpdate'] }} ({{ $APIStatus['rpkiLastUpdateFromNow'] }})</span>
+    </span>
 @endsection
