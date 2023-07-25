e5855ece50aa9546506b21a4e409c0ec95f23df6
David Pratama <davidadityapratama@gmail.com>
2023-07-25 10:19:23 +0700

    fix footer text size on mobile

--- resources/views/components/data-set-last-update.blade.php
+++ resources/views/components/data-set-last-update.blade.php
@@ -1,5 +1,5 @@
 <div>
-     <span class="text-gray-400 text-sm">ROV data last updated
+     <span class="text-gray-400 text-xs md:text-sm">ROV data last updated
         <span class="font-bold">{{ $APIStatus['rpkiLastUpdate'] }} ({{ $APIStatus['rpkiLastUpdateFromNow'] }})</span>
      </span>
 </div>
